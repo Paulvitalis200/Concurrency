d54f51dc5eb72aef6f3958d2fa4d0e8eb79d7361
Paul Otieno <vitalispaul48@live.com>
2026-09-21 08:04:52 +0300

    concurrency: jthreads

--- main.cpp
+++ main.cpp
@@ -9,22 +9,27 @@ void hello() {
 int main() {
     std::thread t(hello);
     t.join();
-    std::cout << "Biri Marung" << std::endl;
 
-    auto myLambda = [](int x) {
+    std::vector<std::jthread> myThreads;
+
+    auto lambda = [](int x) {
         std::cout << "Hello from thread: " << std::this_thread::get_id() << std::endl;
         std::cout << "Argument passed in: " << x << std::endl;
     };
 
-    std::vector<std::thread> myThreads;
-
-    for (int i=0; i < 10; i++) {
-        myThreads.push_back(std::thread(myLambda, i));
-    }
-
     for (int i = 0; i < 10; i++) {
-        myThreads[i].join();
+        myThreads.push_back(std::jthread(lambda, i));
     }
-    
+
+    // for (int i = 0; i < 10; i++) {
+    //     myThreads[i].join();
+    // }
+
+
+
+    std::cout << "This is the main thread" << std::endl;
     return 0;
 }
+
+// To run with jthreads, use C++ version 20. 
+// Use command g++-16 -std=c++20 -o main main.cpp
